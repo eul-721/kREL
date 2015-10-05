4abbc7730cde07ffb565105730e643ea74096189
chaojiac <chaojiac@sfu.ca>
2015-10-04 21:07:55 -0700

    added public

---
+++ public/js/controllers.js
@@ -0,0 +1,10 @@
+kREL.controller('SidebarController',['$scope', function($scope){
+    $scope.mapsIndexes = {
+      "1":[1,2,3,4,5,6],
+      "2":[1,2,3,4,5],
+      "3":[1,2,3,4,5],
+      "4":[1,2,3,4,5],
+      "5":[1,2,3,4,5],
+      "6":[1,2,3]
+    }
+  }]);
